d4382f60c5c2a779a091ff00c82ff79fc45f4179
Elmer Coronel <infcle@gmail.com>
2026-03-20 10:59:28 -0400

    map

---
+++ basic/12-map.js
@@ -0,0 +1,58 @@
+// Map
+
+// Declaración
+
+let myMap = new Map()
+
+console.log(myMap)
+
+// Inicialiación
+
+myMap = new Map([
+    ["name", "Elmer"],
+    ["email", "infcle@gmail.com"],
+    ["age", 37]
+])
+
+console.log(myMap)
+
+// Métodos y propiedades
+
+// set
+
+myMap.set("alias", "infcle")
+myMap.set("name", "Elmer Coronel")
+
+console.log(myMap)
+
+// get
+
+console.log(myMap.get("name"))
+console.log(myMap.get("surname"))
+
+// has
+
+console.log(myMap.has("surname"))
+console.log(myMap.has("age"))
+
+// delete
+
+myMap.delete("email")
+
+console.log(myMap)
+
+// keys, values y entries
+
+console.log(myMap.keys())
+console.log(myMap.values())
+console.log(myMap.entries())
+
+// size
+
+console.log(myMap.size)
+
+// clear
+
+myMap.clear()
+
+console.log(myMap)
